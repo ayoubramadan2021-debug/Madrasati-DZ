e896368c9727c017f20b30093d75f2749c304670
ayoubramadan2021-debug <ayoubramadan2021@gmail.com>
2026-05-30 15:30:14 +0100

    chore: تنظيف shared/components (إبقاء BottomNav + AdminRoute، حذف المهجور)

--- src/shared/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AppButton } from "./AppButton";
-export { default as AppCard } from "./AppCard";
-export { default as AppInput } from "./AppInput";
-export { default as AppTextarea } from "./AppTextarea";
-export { default as AppSelect } from "./AppSelect";
-export { default as LoadingState } from "./LoadingState";
-export { default as ErrorState } from "./ErrorState";
-export { default as EmptyState } from "./EmptyState";
