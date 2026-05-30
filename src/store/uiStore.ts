af99c0342228520191b1415330cc96883f2d0af0
ayoubramadan2021-debug <ayoubramadan2021@gmail.com>
2026-05-30 15:41:12 +0100

    chore: حذف api/ و authStore و uiStore و subjects المهجورة

--- src/store/uiStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useUiStore = create((set) => ({
-  sidebarOpen: false,
-  setSidebarOpen: (value) => set({ sidebarOpen: value }),
-  toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
-}));
